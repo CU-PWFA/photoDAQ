238ea0e2b7b7f0ea030d6ce5926348493a3eb5d0
rariniello <roar3446@colorado.edu>
2018-06-04 13:38:15 -0600

    Added timestamp and metadata functions, also cleaned up some logic.

--- globalVAR.py
+++ globalVAR.py
@@ -9,6 +9,7 @@ Created on Tue Apr  3 14:09:21 2018
 import datetime as dt
 import base64
 import subprocess
+import file
 
 def stringTIME(timeUNIT, powOfTen=1):
     # Convert timeUNIT (integer) to a string with as many place 
@@ -24,6 +25,35 @@ def stringTIME(timeUNIT, powOfTen=1):
             powOfTen-=1
     else:
         return '0'*(powOfTen+1)    
+
+
+def get_date_path():
+    """ Get the path for the current date. 
+    
+    Returns
+    -------
+    path : string
+        The path /year_{}/month{}/day{}/ for today.
+    """
+    date = dt.datetime.now()
+    year = date.year
+    month = stringTIME(date.month) # Do some string formatting
+    day = stringTIME(date.day)     # More string formatting
+    path = '/year_{}/month_{}/day_{}/'.format(year, month, day)
+    return path
+
+
+def get_timestamp():
+    """ Return the current time in timestamp format.
+    
+    Returns
+    -------
+    time : string
+        The timestamp as it goes in the metadata.
+    """
+    date = dt.datetime.now()
+    return writeTimeStamp(date)
+
 
 def writeNewDataSetNum():
     # Writes a new data set number for the present date.
@@ -41,23 +71,21 @@ def getDataSetNum():
     # Number matches today's date then the function returns the 
     # previous Data Set Number + 1.  If the number does not match
     # the date, the function returns a new number that does.
-    with open('META/last_DataSet.txt','r') as file:
-        data = file.readlines()    
-    dataSetPrev = data[5]
+    with open(file.PATH + 'META/DataSet_log.txt','r') as f:
+        data = f.readlines()  
+    f.close()
+    # Handle if this is the first dataset in the log
+    if len(data) == 1:
+        return writeNewDataSetNum()
+    dataSetPrev = data[-1]
     
     lastYEAR = int('20'+dataSetPrev[0:2])
     lastMONTH = int(dataSetPrev[2:4])
     lastDAY = int(dataSetPrev[4:6])
     
     now = dt.datetime.now()
-    if lastYEAR==now.year:
-        if lastMONTH==now.month:
-            if lastDAY==now.day:
-                dataSetNum = int(dataSetPrev)+1
-            else:
-                dataSetNum = writeNewDataSetNum()
-        else:
-            dataSetNum = writeNewDataSetNum()
+    if lastYEAR==now.year and lastMONTH==now.month and lastDAY==now.day:
+        dataSetNum = int(dataSetPrev)+1
     else:
         dataSetNum = writeNewDataSetNum()
         
@@ -66,13 +94,14 @@ def getDataSetNum():
 def advDataSetNum(dataSetNum):
     # Writes dataSetNum as the new Data 
     # Set Number in Meta/last_DataSet.txt
-    with open('META/last_DataSet.txt','r') as file:
-        data = file.readlines()   
-    
-    data[5] = str(dataSetNum)
-    
-    with open('META/last_DataSet.txt','w') as file:
-        file.writelines(data)
+#    with open('META/last_DataSet.txt','r') as file:
+#        data = file.readlines()   
+#    
+#    data[5] = str(dataSetNum)
+#    
+#    with open('META/last_DataSet.txt','w') as file:
+#        file.writelines(data)
+    file.add_to_log(dataSetNum)
 
 def promptREPEAT():
     # Prompts the user to decide if they want to continue 
@@ -128,23 +157,46 @@ def writeMetaDataTxt(dataSetNum, imageDESC, serialNUM, succRATE, timeSTAMP):
     day = stringTIME(now.day)
     fileNAME = 'META/year_{}/month_{}/day_{}/meta_{}.txt'.format(year, month, day, dataSetNum)
     
-    file = open(fileNAME, 'w')
-    file.write('# Description of Data Set\n'
+    f = open(fileNAME, 'w')
+    f.write('# Description of Data Set\n'
                ''+imageDESC+'\n\n'
                '# Camera(s) Used (Name: Serial Number)\n')
     for i in range(len(serialNUM)):
-        file.write(camNAME[serialNUM[i]]+': '+serialNUM[i]+'\n')
-    file.write('\n# Number of Successful Shots\n'
+        f.write(camNAME[serialNUM[i]]+': '+serialNUM[i]+'\n')
+    f.write('\n# Number of Successful Shots\n'
                '{} of {} successful shots\n\n'.format(succRATE[0]-succRATE[1], succRATE[0]))
-    file.write('# Time Stamp of Each Image (year/month/day/hour/minute/second/microsecond)\n')
+    f.write('# Time Stamp of Each Image (year/month/day/hour/minute/second/microsecond)\n')
     for j in range(len(timeSTAMP)):
         if timeSTAMP[j]:
             TS = writeTimeStamp(timeSTAMP[j])
-            file.write('shot {}: {}\n'.format(j+1, TS))
+            f.write('shot {}: {}\n'.format(j+1, TS))
         else:
-            file.write('shot {}: FAIL\n'.format(j+1))
-    file.close()    
+            f.write('shot {}: FAIL\n'.format(j+1))
+    f.close()    
 
 # Camera Name dictionary
 camNAME = {'17529184':'Cam01', '17570564':'Cam02', '17571186':'Cam03', '17583372':'Cam04'}       
 
+
+def create_metadata(arg, startTime):
+    """ Create the metadata dictionary. 
+    
+    Parameters
+    ----------
+    arg : array-like
+        The argument object passed to the main daq function.
+    startTime : string
+        The timestamp data collection started at.
+    
+    Returns
+    -------
+    meta : dict
+        The metadata dictionary for the dataset.
+    """
+    meta = {}
+    meta['INSTR'] = arg[0]
+    meta['Script'] = arg[2]
+    meta['Start time'] = startTime
+    meta['End time'] = get_timestamp()
+    meta['Shots'] = arg[3]
+    
