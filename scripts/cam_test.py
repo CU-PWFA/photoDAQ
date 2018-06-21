d3bb29974fa303cf7c931d9ec4925468388453a1
rariniello <roar3446@colorado.edu>
2018-06-21 17:23:21 -0600

    Updated dev notebook and test script.

--- scripts/cam_test.py
+++ scripts/cam_test.py
@@ -8,19 +8,24 @@ Created on Fri Jun 15 10:21:46 2018
 
 import time
 
-cam = {}
+devices = {}
 
 def setup(instr):
-    global cam
-    cam = instr['Camera']
-    cam.start_capture()
+    global devices
+    devices = instr
+    for name in instr:
+        if instr[name].type == 'Camera':
+            cam = instr[name]
+            cam.start_capture()
     
 def measure(i):
-    image = cam.retrieve_buffer()
-    ret = {
-            'Camera' : {
-                    'meta'  : {},
-                    'image' : image
-                    }
-            }
+    ret = {}
+    for name in devices:
+        if devices[name].type == 'Camera':
+            cam = devices[name]
+            image = cam.retrieve_buffer()
+            ret[name] = {
+                        'meta'  : {},
+                        'image' : image
+                        }
     return ret
