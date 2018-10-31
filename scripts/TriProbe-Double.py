2d3faa609caa88afa272e4215fc5d06c2b202933
Michael <mige4603@colorado.edu>
2018-10-30 22:46:01 -0600

    Analysis Update

--- scripts/TriProbe-Double.py
+++ scripts/TriProbe-Double.py
@@ -11,77 +11,47 @@ sys.path.append('../')
 import numpy as np
 import matplotlib.pyplot as plt
 import file as f
+from scipy.optimize import curve_fit
 
-def line(X, m, b):
-    npts = len(X)
-    Y = np.empty(npts)
-    for i in range(npts):
-        Y[i] = m*X[i]+b
-    return Y
+from matplotlib import rc
+rc('mathtext', default='regular')
+
+def line(x, m, b):
+    return m*x + b
 
 f.PATH = f.get_file_path()
+path = 'TRACE/year_2018/month_10/day_19/'
 
-cnt = 0
-shot = 1
-lng = 57
-dataset = '1810080036'
+file = 'IV-Curve_SET_1-3.npy'
+data = np.load(f.PATH+path+file).item()
 
-V_set = np.empty(lng)
-I_set = np.empty(lng)
+crnt = data['Currnt']
+volt = data['Voltage']
 
-while shot<=113:
-    SET = f.load_SET('ttyUSB0', dataset, shot)
-    TRACE = f.load_TRACE('TDS2024C', dataset, shot)
-    
-    v2 = np.max(TRACE['y'])
-    v3 = SET['voltage']
-    
-    I_set[cnt] = v2/10.
-    V_set[cnt] = v3
-    
-    cnt+=1
-    shot=2*cnt+1
-    
-V_p1 = 10
-I_p1 = I_set[np.where(V_set==V_p1)][0]
-    
-Vo = V_set[(V_set <= V_p1)]
-Io = I_set[0:len(Vo)]
-m, b = np.polyfit(Vo, Io, 1)
+Vp1 = 3
+Vp2 = -3
 
-I_e2 = I_p1 - b
-G = I_e2/(2*I_p1)
-R = 1./m
-temp = round((G-G*G)*R*2*I_p1, 2)
+Ip1 = crnt[np.round(volt, 1)==Vp1]
+Ip2 = -crnt[np.round(volt, 1)==Vp2]
 
-O_set = line(Vo, m, b)
+I_set = crnt[(crnt > -Ip2) & (crnt < Ip1)]
+V_set = volt[(crnt > -Ip2) & (crnt < Ip1)]
+Ie2 = I_set + Ip2
 
-Vf = V_set[(V_set > V_p1)]
-If = I_set[len(I_set)-len(Vf):len(I_set)]
-m, b = np.polyfit(Vf, If, 1)
-F_set = line(Vf, m, b)
-    
+log = np.log((Ip1+Ip2)/Ie2 - 1)
+popt, pcov = curve_fit(line, V_set, log)
+
 fig = plt.figure()
 ax = fig.add_subplot(111)
 fnt = 14
-c='k'
 
-ax.plot(V_set, I_set, color=c)
-ax.plot(Vo, O_set, color=c, ls='--')
-ax.plot(Vf, F_set, color=c, ls='--')
+vplot = np.linspace(Vp2, Vp1, 100)
+ax.scatter(V_set, log, s=10, c='b')
+plt.plot(vplot, line(vplot, *popt), color='k')
+ax.set_xlabel('Bias Voltage (V)', color=c, fontsize=fnt)
+ax.set_ylabel('$\ln (\Gamma)$', color=c, fontsize=fnt)
+ax.set_title('Temp: %0.2f eV' % (-1/popt[0]), color=c, fontsize=fnt)
 
-ax.set_ylabel('$I_{13} \ (mA)$', color=c, fontsize=fnt)
-ax.set_xlabel('$V_{d3} \ (V)$', color=c, fontsize=fnt)
-
-#ax.set_ylim(0, np.max(I_set))
-
-ax.spines['bottom'].set_color(c)
-ax.spines['top'].set_color(c)
-ax.tick_params(axis='x', colors=c, labelsize=fnt-2)
-ax.tick_params(axis='y', colors=c, labelsize=fnt-2)
-ax.set_title('Temp: {} eV'.format(temp), color=c, fontsize=fnt)
-
-plt.legend()
 plt.tight_layout()
-path = f.get_dirName_from_dataset('TRACE', dataset)
-plt.savefig(path+'/TriProbPlot-Double.png')
+#plt.show()
+plt.savefig(f.PATH+path+'IV_TEMP_1-3.png')
