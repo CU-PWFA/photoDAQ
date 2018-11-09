c0d010e32cd366a2f92fb896855ddc13ed52b7c7
lsx249 <30448133+lsx249@users.noreply.github.com>
2018-11-09 12:27:12 +0800

    Update HR4000.py

--- processes/HR4000.py
+++ processes/HR4000.py
@@ -9,5 +9,16 @@ Created on Wed Jul 11 18:03:07 2018
 from processes.process import Process
 
 class HR4000(Process):
+    def save_spectrum(self):
+        """ Save spectrum 
+        file=the filename in which you want to save
+        """        
+        np.save(file,self.get_spectrum)
+    
+    def show_spectrum_plot(self):
+        """ Show the plot of spectral lines"""
+        plt.plot(self.get_wavelength,self.get_intensity)
+    
+    def live_stream_spectro():
     pass
 
