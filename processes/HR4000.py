7faef347d717e2ac10310a98918e583cef4993f0
cu-pwfa <Michael Gerard>
2018-12-18 11:57:11 -0700

    Added a save waveform function to the HR4000 process.

--- processes/HR4000.py
+++ processes/HR4000.py
@@ -9,5 +9,23 @@ Created on Wed Jul 11 18:03:07 2018
 from processes.process import Process
 
 class HR4000(Process):
-    pass
+    def save_spectrum(self):
+        """ Save the current spectrum to disk. """
+        w, a = self.device.get_spectrum()
+        meta = self.create_meta()
+        data = {'save' : True,
+                'meta' : meta,
+                't' : w,
+                'y' : a}
+        self.shot += 1
+        self.r_queue.put(data)
+        
+    
+    def get_datatype(self):
+        """ Return the type of data. """
+        return "TRACE"
+    
+    def get_type(self):
+        """ Return the instrument type. """
+        return "HR4000"
 
