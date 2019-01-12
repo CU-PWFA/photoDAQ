41de6cec1f7f5b4ccc2adb43e0326c3813b05d5f
rariniello <roar3446@colorado.edu>
2019-01-11 18:17:35 -0700

    Created windows for spectrometer added content to the camera.

--- windows/camera.py
+++ windows/camera.py
@@ -13,6 +13,8 @@ from matplotlib.backends.backend_qt4agg import (
     FigureCanvasQTAgg as FigureCanvas,
     NavigationToolbar2QT as NavigationToolbar)
 from matplotlib.figure import Figure
+import pyqtgraph
+import pyqtgraph as pg
 import matplotlib.pyplot as plt
 import numpy as np
 import threading
@@ -22,6 +24,8 @@ qtCreatorFile = "windows/camera.ui"
 Ui_CameraWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
 class CameraWindow(QtBaseClass, Ui_CameraWindow):
+    data_acquired = pyqtSignal(dict)
+    
     def __init__(self, parent, DAQ, serial, queue):
         """ Create the parent class and add event handlers. 
         
@@ -43,77 +47,103 @@ class CameraWindow(QtBaseClass, Ui_CameraWindow):
         # Add event handlers to all the buttons
         self.startStreamButton.clicked.connect(self.start_stream)
         self.stopStreamButton.clicked.connect(self.stop_stream)
+        self.shutterField.valueChanged.connect(self.set_shutter)
+        self.gainField.valueChanged.connect(self.set_gain)
+        self.framerateField.valueChanged.connect(self.set_framerate)
+        self.brightnessField.valueChanged.connect(self.set_brightness)
+        self.data_acquired.connect(self.update_canvas)
+        self.data_acquired.connect(self.update_info)
         
         # Grab references for controlling the camera
         self.DAQ = DAQ
         self.serial = serial
         self.queue = queue
-        th = Thread(queue, parent=self)
-        th.changePixmap.connect(self.setPixMap)
-        th.start()
-        # Create the matplotlib container for the image
-        #self.create_image()
-        #self.create_update_thread()
+
+        # Create the container for the image
+        self.create_image()
+        self.create_update_thread()
+        self.setWindowTitle(str(serial))
+        self.set_field_range()
         
     def create_image(self):
         """ Create the matplotlib image canvas. """
-        self.fig = fig = Figure()
-        self.im_ax = im_ax = plt.Axes(fig, [0., 0., 1., 1.])
-        im_ax.set_axis_off()
-        fig.add_axes(im_ax)
-        self.image = im_ax.imshow([[0, 0], [0, 0]], aspect='equal', animated=True)
-        
-        # The mplvl is a named layout on the imageWidget
-        self.canvas = canvas = FigureCanvas(fig)
-        self.mplvl.addWidget(canvas)
-        canvas.draw()
-        self.toolbar = NavigationToolbar(self.canvas, self.imageWidget,
-                                         coordinates=True)
-        self.mplvl.addWidget(self.toolbar)
+        # Following the pyqtgraph VideoSpeedTest example, add the image
+        pyqtgraph.setConfigOptions(imageAxisOrder='row-major')
+        self.vb =  pg.ViewBox()
+        self.graphicsView = pg.GraphicsView(self.imageWidget)
+        self.mplvl.addWidget(self.graphicsView)
+        self.graphicsView.setCentralItem(self.vb)
+        self.vb.setAspectLocked()
+        self.image_view = pg.ImageItem()
+        self.vb.addItem(self.image_view)
         
     def create_update_thread(self):
         """ Create a thread to poll the response queue and update the image. """
-        args = (self.queue,)
-        thread = threading.Thread(target=self.update_thread, args=args)
+        args = (self.queue, self.data_acquired.emit)
+        thread = threading.Thread(target=self.update_thread, args=args, name='CameraUpdateThread')
         thread.setDaemon(True)
         thread.start()
          
-    def update_thread(self, queue):
+    def update_thread(self, queue, callback):
         """ Wait for updated data to display. 
         
         Parameters
         ----------
         queue : queue
             The queue that the image data is arriving on.
+        callback : func
+            The signal.emit to call to update the plot. 
         """
         while True:
             data = queue.get()
-            if data == 'Connected':
-                self.enable_window()
+            if data == '__exit__':
+                break
+            elif data == '__Connected__':
+                self.setup_window()
             else:
-                self.update_canvas(data)
+                callback(data)
             queue.task_done()
-            
-    def update_canvas(self, data):
-        """ Update the canvas with the new image. 
         
-        Parameters
-        ----------
-        data : dict
-            The dictionary with the image data and meta.
-        """
-        self.im_ax.clear()
-        self.im_ax.set_axis_off()
-        # XXX set_array or set_data should work, but they don't
-        #self.image.set_array(data['raw'])
-        self.im_ax.imshow(data['raw'], aspect='equal')
-        self.canvas.draw()
+    def setup_window(self):
+        """ Perform setup after the camera connects. """
+        self.startStreamButton.setEnabled(True)
+        self.stopStreamButton.setEnabled(True)
+        self.shutterField.setEnabled(True)
+        self.gainField.setEnabled(True)
+        self.framerateField.setEnabled(True)
+        self.brightnessField.setEnabled(True)
         
-    def enable_window(self):
-        """ Enable control of the instrument after it connects. """
-        pass
+    def set_field_range(self):
+        """ Set the range of the spinbox fields based on the camera serial. """
+        # TODO should probably just grab this from the cameras
+        serial = self.serial
+        bounds = {17583372 : {'ShutterMin' : 0.0477,
+                              'GainMax' : 47.99,
+                              'BrightnessMin' : 0.0,
+                              'BrightnessMax' : 12.47},
+                  17571186 : {'ShutterMin' : 0.039,
+                              'GainMax' : 18,
+                              'BrightnessMin' : 0.0,
+                              'BrightnessMax' : 25},
+                  17529184 : {'ShutterMin' : 0.0596,
+                              'GainMax' : 47.99,
+                              'BrightnessMin' : 0.0,
+                              'BrightnessMax' : 12.475},
+                  17570564 : {'ShutterMin' : 0.0456,
+                              'GainMax' : 23.99,
+                              'BrightnessMin' : 1.368,
+                              'BrightnessMax' : 7.42}
+                  }
+        self.shutterField.setMinimum(bounds[serial]['ShutterMin'])
+        self.gainField.setMaximum(bounds[serial]['GainMax'])
+        # The device is connected yet so we can't update it's parameters
+        self.brightnessField.blockSignals(True)
+        self.brightnessField.setRange(bounds[serial]['BrightnessMin'], 
+                                      bounds[serial]['BrightnessMax'])
+        self.brightnessField.setValue(bounds[serial]['BrightnessMin'])
+        self.brightnessField.blockSignals(False)
         
-    def send_command(self, command, *args):
+    def send_command(self, command, args=None, kwargs=None):
         """ Send commands to this windows instruments. 
         
         Parameters
@@ -136,31 +166,53 @@ class CameraWindow(QtBaseClass, Ui_CameraWindow):
     @pyqtSlot()
     def stop_stream(self):
         """ Stop the camera stream and the display. """
-        # It appears that the stop capture does not actually stop the data from the camera
         self.send_command('stop_stream')
         
-    @pyqtSlot(QImage)
-    def setPixMap(self, p):
-        p = QPixmap.fromImage(p)    
-        self.label.setPixmap(p)
+    @pyqtSlot(float)
+    def set_shutter(self, value):
+        """ Set the shutter value. """
+        self.send_command('set_shutter_settings', (None, value))
+        
+    @pyqtSlot(float)
+    def set_gain(self, value):
+        """ Set the gain value. """
+        self.send_command('set_gain_settings', (None, value))
+        
+    @pyqtSlot(float)
+    def set_framerate(self, value):
+        """ Set the framerate value. """
+        self.send_command('set_frame_rate', (None, value))
+        
+    @pyqtSlot(float)
+    def set_brightness(self, value):
+        """ Set the brightness value. """
+        self.send_command('set_brightness_settings', (value,))
     
-       
-class Thread(QThread):
-    changePixmap = pyqtSignal(QImage)
-    def __init__(self, queue, parent=None):
-        super().__init__(parent=parent)
-        self.queue = queue
+    @pyqtSlot(dict)
+    def update_canvas(self, data):
+        """ Update the canvas with the new image. 
+        
+        Parameters
+        ----------
+        data : dict
+            The dictionary with the image data and meta.
+        """
+        self.image_view.setImage(data['raw'], autoLevels=False)
+    
+    @pyqtSlot(dict)
+    def update_info(self, data):
+        """ Update the info blocks. 
+        
+        Parameters
+        ----------
+        data : dict
+            The dictionary with the image data and meta.
+        """
+        DAQ = self.DAQ
+        serial = self.serial
+        self.r_queueNum.setNum(DAQ.response_queue[serial].qsize())
+        self.s_queueNum.setNum(DAQ.save_queue[serial].qsize())
 
-    def run(self):
-        while True:
-            data = self.queue.get()
-            if data == 'Connected':
-                pass
-            else:
-                width, height = data['meta']['pixel']
-                rawImage = QImage(data['raw'], width, height, QImage.Format_Indexed8)
-                self.changePixmap.emit(rawImage)
-            self.queue.task_done()
 # For testing the window directly
 if __name__ == "__main__":
     import sys
